14989c858307bdf8b73a1f2a91439c8fee4600d8
E2517 <61824329+E2517@users.noreply.github.com>
2020-08-11 11:58:24 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
     const { getByText } = render( < App / > );
-    const linkElement = getByText(/e2517/i);
+    const linkElement = getByText(/ñññññ/);
     expect(linkElement).toBeInTheDocument();
 });
